0d8259dcd1b9bda54e524dc39956f768c9ffd81d
Szabolcs Makai <git@raggedgenes.com>
2019-05-16 21:29:07 +0200

    Colored links 2

--- src/lines/WireAccessor.js
+++ src/lines/WireAccessor.js
@@ -3,10 +3,13 @@
  * 
  * Wires are "lines" with 1.0 width.
  */
+
+import Color from '../Color';
 class WireAccessor {
-  constructor(buffer, offset) {
+  constructor(buffer, offset, color) {
     this.offset = offset;
     this.buffer = buffer;
+    this.color = color || new Color(1, 1, 1, 1); 
   }
 
   update(from, to) {
@@ -16,8 +19,21 @@ class WireAccessor {
     buffer[offset + 0] = from.x
     buffer[offset + 1] = from.y
 
-    buffer[offset + 2] = to.x
-    buffer[offset + 3] = to.y
+    buffer[offset + 5] = to.x
+    buffer[offset + 6] = to.y
+
+    this.setColor(this.color);
+  }
+  setColor(color) {
+    this.color = color;
+    var buffer = this.buffer;
+    var offset = this.offset;
+    buffer[offset + 2] = color.r
+    buffer[offset + 3] = color.g
+    buffer[offset + 4] = color.b
+    buffer[offset + 7] = color.r
+    buffer[offset + 8] = color.g
+    buffer[offset + 9] = color.b
   }
 }
 
